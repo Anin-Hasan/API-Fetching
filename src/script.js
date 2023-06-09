8c28b0b626fedd72479994c886c78523b71b1253
Anin-Hasan <200921@ku.ac.bd>
2023-06-09 20:02:28 +0600

    added genres

--- src/script.js
+++ src/script.js
@@ -1,62 +1,157 @@
 const API_KEY = "api_key=6d67021662c3fe14209fe92c098382d6";
 const BASE_URL = "https://api.themoviedb.org/3";
-const API_URL = BASE_URL+"/discover/movie?sort_by=popularity.desc&"+API_KEY;
+const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
-const searchUrl = BASE_URL + "/search/movie?"+API_KEY;
+const searchUrl = BASE_URL + "/search/movie?" + API_KEY;
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 const titleBtn = document.getElementById("titleBtn");
+const genreSegment = document.getElementById("genreSegment");
 
+//genre API fetching
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDY3MDIxNjYyYzNmZTE0MjA5ZmU5MmMwOTgzODJkNiIsInN1YiI6IjY0N2Y3YjUwY2E3ZWM2MDEzOTAwM2M0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SHKgRO855Lu-KlPrIQ3ga_LPeh68FnPDEF39Moz_cXo",
+  },
+};
+
+fetch("https://api.themoviedb.org/3/genre/movie/list?language=en", options)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response.genres);
+    getGenres(response.genres);
+  })
+  .catch((err) => console.error(err));
+
+function getGenres(Arrays) {
+  Arrays.forEach((array) => {
+    const { id, name } = array;
+    const genreBtn = `
+            <button id="${id}" onclick="myFunction(${id})"class=" gBtn flex items-center bg-sky-600 h-8 mx-2 my-2 px-2 rounded-md text-white font-semibold hover:scale-105 duration-300 shadow-md hover:shadow-button">
+            <h1>${name}</h1>
+            </button>
+        `;
+    genreSegment.innerHTML += genreBtn;
+  });
+}
+function home(){
+
+  main.innerHTML = "";
+  clickColor(0);
+  getMovies(API_URL);
+
+}
 
 getMovies(API_URL);
 
-function getMovies(url){
-    fetch(url).then(res=>res.json()).then(data => {
-        console.log(data.results)
-        showMovies(data.results)
-    })
+function getMovies(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      //console.log(data.results);
+      showMovies(data.results);
+      //getMatched(data.results);
+    });
 }
-function showMovies(data){
-    data.forEach(movie => {
-        const {title, overview, poster_path, vote_average } = movie;
-               
-        movieEl = document.createElement("div");
-        movieEl.classList.add("relative");
-        movieEl.innerHTML = `
-        <img src=${IMG_URL+ poster_path} alt="">
+
+function clickColor(id){
+  const buttonCollection = genreSegment.getElementsByTagName("button");
+  //console.log(buttonCollection);
+  Array.from(buttonCollection).forEach((btn) => {
+    //console.log(btn)
+    if (btn.id == id) {
+      if(btn.style.backgroundColor === "orange"){
+        console.log("doble clickd")
+        home();
+        throw new Error("");
+
+      }
+      else{
+
+        btn.style.backgroundColor = "orange";
+      }
+    }
+    else{
+      btn.style.backgroundColor = "rgb(2 132 199)";
+    }
+  });
+  // const clicked = document.getElementById(id);
+
+}
+
+function myFunction(id) {
+  
+  
+  clickColor(id);
+
+  fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      //console.log(data.results);
+      //showMovies(data.results);
+      getMatched(data.results, id);
+    });
+  //console.log(id);
+}
+
+
+
+function showMovies(data) {
+  data.forEach((movie) => {
+    const { title, overview, poster_path, vote_average } = movie;
+
+    movieEl = document.createElement("div");
+    movieEl.classList.add("relative");
+    movieEl.innerHTML = `
+        <img src=${IMG_URL + poster_path} alt="">
         <div class="flex justify-between">
             <h1 class="font-bold">${title}</h1>
             <h1 class="font-bold text-orange-600" >${vote_average}</h1>
 
         </div>
 
-        <div class="absolute w-54 right-0 bottom-0 left-0 bg-white opacity-0 -translate-y-6 hover:opacity-90 duration-300 text-sm p-8  ">
+        <div class="overflow-x-scroll absolute w-54 right-0 bottom-0 left-0 bg-white opacity-0 -translate-y-6 hover:opacity-90 duration-300 text-sm p-8  ">
             <h1 class="font-semibold">Overview</h1>
             <p>${overview}</p>
         </div>
-        `
-        main.appendChild(movieEl);
+        `;
+    main.appendChild(movieEl);
+  });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  main.innerHTML = "";
+  const searchTerm = search.value;
+  console.log(searchUrl);
+  if (searchTerm) {
+    getMovies(searchUrl + "&query=" + searchTerm);
+  }
+});
+
+titleBtn.addEventListener("click",home);
+
+
+
+function getMatched(data, id) {
+  const catagorizedData = [];
+  data.forEach((element) => {
+    const { genre_ids } = element;
+    //console.log(genre_ids);
+    genre_ids.forEach((e) => {
+      if (e == id) {
+        catagorizedData.push(element);
+      }
     });
-
-
-}
-
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
     main.innerHTML = "";
-    const searchTerm = search.value;
-    console.log(searchUrl)
-    if(searchTerm){
-        getMovies(searchUrl + "&query=" +searchTerm);
-    }
-
-})
-
-titleBtn.onclick=()=>{
-    main.innerHTML = "";
-    getMovies(API_URL)
+    showMovies(catagorizedData);
+    //console.log(genre_ids.find(e=>{e == id}));
+    //console.log(data.find(c=>{c.genre_ids.find(e=>{e==id})}));
+  });
+  //console.log(catagorizedData)
 }
-
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow:{
+        navbar:"0 1px 1px rgba(0,0,0,0.11), 0 2px 2px rgba(0,0,0,0.11), 0 4px 4px rgba(0,0,0,0.11), 0 8px 8px rgba(0,0,0,0.11), 0 16px 16px rgba(0,0,0,0.11), 0 32px 32px rgba(0,0,0,0.11)",
+        button:"0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
+      },
+    },
   },
   plugins: [],
 }
